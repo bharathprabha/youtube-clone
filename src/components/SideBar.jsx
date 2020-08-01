5dd72dadafca4c044f285d93a730c4bfbb696de5
Bharath Prabha <bharathprabhakaran28@gmail.com>
2020-08-01 20:19:58 +0530

    Dark Mode added.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,13 +3,28 @@ import "./SideBar.css";
 import HomeIcon from "@material-ui/icons/Home";
 import WhatshotIcon from "@material-ui/icons/Whatshot";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import LibraryAddIcon from "@material-ui/icons/LibraryAdd";
+import HistoryIcon from "@material-ui/icons/History";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import MenuOpenIcon from "@material-ui/icons/MenuOpen";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+
 import SideBarRow from "./SideBarRow";
+
 const SideBar = () => {
   return (
     <div className="SideBar">
       <SideBarRow selected Icon={HomeIcon} title={"Home"} />
       <SideBarRow Icon={WhatshotIcon} title={"Trending"} />
       <SideBarRow Icon={SubscriptionsIcon} title={"Sub"} />
+      <hr />
+      <SideBarRow Icon={LibraryAddIcon} title={"Library"} />
+      <SideBarRow Icon={HistoryIcon} title={"History"} />
+      <SideBarRow Icon={VideoLibraryIcon} title={"Your videos"} />
+      <SideBarRow Icon={AccessTimeIcon} title={"Watch later"} />
+      <SideBarRow Icon={MenuOpenIcon} title={"Popular videos"} />
+      <SideBarRow Icon={ThumbUpAltIcon} title={"Liked videos"} />
       <hr />
     </div>
   );
