29d3533c420777879623979caa9e912889d38efe
Bharath Prabha <bharathprabhakaran28@gmail.com>
2020-08-01 17:22:21 +0530

    Started for Recommended.

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,7 +1,62 @@
 import React from "react";
 import "./Posts.css";
+import VideoCards from "./VideoCards";
 const Posts = () => {
-  return <div className="posts">hello post</div>;
+  return (
+    <div className="posts">
+      <h2>Recommended</h2>
+      <div className="posts__videocards">
+        <VideoCards
+          image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+          title="The first  video on youtube"
+          chennal="First chennal"
+          views="3.4 M"
+          timestamp="4 months ago"
+          duration="3:50"
+        />
+        <VideoCards
+          image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+          title="The first  video on youtube"
+          chennal="First chennal"
+          views="3.4 M"
+          timestamp="4 months ago"
+          duration="3:50"
+        />
+        <VideoCards
+          image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+          title="The first  video on youtube"
+          chennal="First chennal"
+          views="3.4 M"
+          timestamp="4 months ago"
+          duration="3:50"
+        />
+        <VideoCards
+          image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+          title="The first  video on youtube"
+          chennal="First chennal"
+          views="3.4 M"
+          timestamp="4 months ago"
+          duration="3:50"
+        />
+        <VideoCards
+          image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+          title="The first  video on youtube"
+          chennal="First chennal"
+          views="3.4 M"
+          timestamp="4 months ago"
+          duration="3:50"
+        />
+        <VideoCards
+          image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
+          title="The first  video on youtube"
+          chennal="First chennal"
+          views="3.4 M"
+          timestamp="4 months ago"
+          duration="3:50"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Posts;
---
+++ src/components/VideoCards.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./VideoCards.css";
+import Avatar from "@material-ui/core/Avatar";
+const VideoCards = ({ image, title, chennal, views, timestamp, duration }) => {
+  return (
+    <div className="VideoCard">
+      <img className="VideoCard__Thumnill" src={image} alt="" />
+      <div className="VideoCard__info">
+        <Avatar className="VideoCard__Avatar" />
+        <div className="VideoCard__text"></div>
+        <h4>{title}</h4>
+        <p>{chennal}</p>
+        <p>
+          {views} * {timestamp} 
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default VideoCards;
