eb6619744939db9e381ea83846b7f9982c5a529e
Bharath Prabha <bharathprabhakaran28@gmail.com>
2020-08-01 19:32:30 +0530

    completed with video portions.

--- src/components/VideoCards.jsx
+++ src/components/VideoCards.jsx
@@ -7,12 +7,14 @@ const VideoCards = ({ image, title, chennal, views, timestamp, duration }) => {
       <img className="VideoCard__Thumnill" src={image} alt="" />
       <div className="VideoCard__info">
         <Avatar className="VideoCard__Avatar" />
-        <div className="VideoCard__text"></div>
-        <h4>{title}</h4>
-        <p>{chennal}</p>
-        <p>
-          {views} * {timestamp} 
-        </p>
+        <div className="VideoCard__text">
+          {" "}
+          <h4>{title}</h4>
+          <p>{chennal}</p>
+          <p>
+            {views} * {timestamp}
+          </p>
+        </div>
       </div>
     </div>
   );
