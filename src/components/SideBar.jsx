844383ad938df3882a9bd330e811ff62d544f920
Bharath Prabha <bharathprabhakaran28@gmail.com>
2020-08-01 21:04:20 +0530

    Customized SideBar.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -9,6 +9,15 @@ import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import MenuOpenIcon from "@material-ui/icons/MenuOpen";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import LocalMoviesIcon from "@material-ui/icons/LocalMovies";
+import SportsEsportsIcon from "@material-ui/icons/SportsEsports";
+import LiveTvIcon from "@material-ui/icons/LiveTv";
+import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
+import SettingsIcon from "@material-ui/icons/Settings";
+import FlagIcon from "@material-ui/icons/Flag";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
 
 import SideBarRow from "./SideBarRow";
 
@@ -26,6 +35,16 @@ const SideBar = () => {
       <SideBarRow Icon={MenuOpenIcon} title={"Popular videos"} />
       <SideBarRow Icon={ThumbUpAltIcon} title={"Liked videos"} />
       <hr />
+      <SideBarRow Icon={YouTubeIcon} title={"YouTube Premium"} />
+      <SideBarRow Icon={LocalMoviesIcon} title={"Movies"} />
+      <SideBarRow Icon={SportsEsportsIcon} title={"Gaming"} />
+      <SideBarRow Icon={LiveTvIcon} title={"Live"} />
+      <SideBarRow Icon={EmojiObjectsIcon} title={"Learning"} />
+      <SideBarRow Icon={SettingsIcon} title={"Settings"} />
+      <hr />
+      <SideBarRow Icon={FlagIcon} title={"Report History"} />
+      <SideBarRow Icon={HelpOutlineIcon} title={"Help"} />
+      <SideBarRow Icon={EmojiFlagsIcon} title={"Send Feedback"} />
     </div>
   );
 };
