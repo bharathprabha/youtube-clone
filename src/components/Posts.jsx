b88cf29915e79532980656e569f79e81321ad4d8
Bharath Prabha <bharathprabhakaran28@gmail.com>
2020-08-02 19:32:57 +0530

    Little tweeks on key for post.

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -8,7 +8,7 @@ const Posts = () => {
   const [Videos, setVideos] = useState([]);
   useEffect(() => {
     db.collection("videos").onSnapshot((snopshot) => {
-      setVideos(snopshot.docs.map((doc) => doc.data()));
+      setVideos(snopshot.docs.map((doc) => ({ post: doc.data(), id: doc.id })));
     });
   }, []);
   console.log(Videos);
@@ -17,7 +17,7 @@ const Posts = () => {
       <h2>Recommended</h2>
       <div className="posts__videocards">
         {Videos.map((videos) => (
-          <VideoCards key={videos.title} videoInfo={videos} />
+          <VideoCards key={videos.id} videoInfo={videos.post} />
         ))}
       </div>
     </div>
