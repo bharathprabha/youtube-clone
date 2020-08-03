1d3c4f01e116ece01e72f21fa8455d4e393e4f7a
Bharath Prabha <bharathprabhakaran28@gmail.com>
2020-08-03 17:06:28 +0530

    Updates

---
+++ precache-manifest.5884f7526acdd4a93e1c5a7fa0c4aa4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8713f8b01fcc0aea9f9e07eb88a49dec",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "b70fff3a804dde6f9e39",
+    "url": "/youtube-clone/static/css/main.6b91f225.chunk.css"
+  },
+  {
+    "revision": "4e56201383d3e91894c4",
+    "url": "/youtube-clone/static/js/2.4a4c34cb.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/youtube-clone/static/js/2.4a4c34cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b70fff3a804dde6f9e39",
+    "url": "/youtube-clone/static/js/main.7e6fee11.chunk.js"
+  },
+  {
+    "revision": "8a09cd451cb85b60110f",
+    "url": "/youtube-clone/static/js/runtime-main.a270c926.js"
+  }
+]);
